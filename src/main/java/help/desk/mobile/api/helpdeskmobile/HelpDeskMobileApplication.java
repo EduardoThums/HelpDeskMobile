944724dafc7420c24706f7bcfc5b30a442668829
eduardo.thums <eduardo.thums@cwi.com.br>
2019-09-30 14:20:10 -0300

    Add genesis script and project structure

---
+++ src/main/java/help/desk/mobile/api/helpdeskmobile/HelpDeskMobileApplication.java
@@ -0,0 +1,13 @@
+package help.desk.mobile.api.helpdeskmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/help/desk/mobile/api/helpdeskmobile/HelpDeskMobileApplicationTests.java
@@ -0,0 +1,16 @@
+package help.desk.mobile.api.helpdeskmobile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelpDeskMobileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

50204a708505bf6f76f15bc1c2a81d99f63859e2
Leonardo Berlatto <leoberlatto@gmail.com>
2019-10-17 00:12:18 -0300

    Add basic login activity

--- android-app/app/src/main/java/help/desk/mobile/app/activity/LoginActivity.java
+++ android-app/app/src/main/java/help/desk/mobile/app/activity/LoginActivity.java
@@ -1,12 +1,18 @@
 package help.desk.mobile.app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.helpdeskmobile.R;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.login_button) private Button loginButton;
+    @BindView(R.id.login_email_edit_text) private EditText emailEditText;
+    @BindView(R.id.login_password_edit_text) private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

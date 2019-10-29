9d4aefbf0ad88b7839705d898101549469e05485
eduardo.thums <eduardo.thums@cwi.com.br>
2019-10-29 14:53:41 -0300

    Add FindAllAreasService unit test

---
+++ api/src/test/java/help/desk/mobile/api/service/area/FindAllAreasServiceTest.java
@@ -0,0 +1,54 @@
+package help.desk.mobile.api.service.area;
+
+import help.desk.mobile.api.AbstractUnitTest;
+import help.desk.mobile.api.controller.area.response.AreaDetailsResponse;
+import help.desk.mobile.api.domain.entity.AreaEntity;
+import help.desk.mobile.api.mapper.AreaMapper;
+import help.desk.mobile.api.repository.area.AreaRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author eduardo.thums
+ */
+public class FindAllAreasServiceTest extends AbstractUnitTest {
+
+	@InjectMocks
+	private FindAllAreasService findAllAreasService;
+
+	@Mock
+	private AreaRepository areaRepository;
+
+	@Mock
+	private AreaMapper areaMapper;
+
+	@Test
+	public void findAllAreas() {
+		// Arrange
+		final AreaEntity mockedAreaEntity = new AreaEntity();
+
+		final AreaDetailsResponse mockedResponse = new AreaDetailsResponse();
+
+		BDDMockito.given(areaRepository.findAll())
+				.willReturn(Collections.singletonList(mockedAreaEntity));
+
+
+		BDDMockito.given(areaMapper.toAreaDetailsResponse(Collections.singletonList(mockedAreaEntity)))
+				.willReturn(Collections.singletonList(mockedResponse));
+
+		// Act
+		final List<AreaDetailsResponse> responseList = findAllAreasService.findAllAreas();
+
+		// Assert
+		Assert.assertFalse(responseList.isEmpty());
+
+		final AreaDetailsResponse response = responseList.get(0);
+		Assert.assertEquals(mockedResponse, response);
+	}
+}
